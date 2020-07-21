f16e44cd39f25e2c2a3521b6bac03c32c3dbad9f
rms355 <rms355@gmail.com>
2020-07-21 00:26:24 -0500

    loading page

--- index.js
+++ index.js
@@ -6,12 +6,30 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 
+app.use('/static', express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send('index.htm');
-});
+let handleRequest = (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.htm', null, function (error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('File not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+};
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log("Hello World");
-});
+// app.get('/', (req, res) => {
+//     res.send("Welcome");
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//     console.log("Hello World");
+// });
+
+http.createServer(handleRequest).listen(3000);
