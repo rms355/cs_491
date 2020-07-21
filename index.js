4128fa8f02b793707088735755b4d89c6df34918
rms355 <rms355@gmail.com>
2020-07-21 01:30:30 -0500

    /pages work

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('public'))
-app.use('/static', express.static('public'))
+app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static('pages'));
+app.use('/static', express.static('pages'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.htm'));
