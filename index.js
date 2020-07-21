1d82da8891c30bcbd1a50a46eea07854d1590f5d
rms355 <rms355@gmail.com>
2020-07-20 23:24:41 -0500

    added more const

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const nodemailer = require('nodemailer');
+
 const app = express();
+
 
 app.get('/', (req, res) => {
     res.send("Welcome");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Hell World");
 });
