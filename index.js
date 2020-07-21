0f3dabe83ffc26d399bcf40281fd788a75009e2a
rms355 <rms355@gmail.com>
2020-07-21 00:36:04 -0500

    Home page loading

--- index.js
+++ index.js
@@ -3,33 +3,21 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const nodemailer = require('nodemailer');
+const path = require('path');
 
 const app = express();
 
 app.use('/static', express.static('public'))
 
-let handleRequest = (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.htm', null, function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('File not found!');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
-};
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.htm'));
+});
 
 // app.get('/', (req, res) => {
 //     res.send("Welcome");
 // });
 
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//     console.log("Hello World");
-// });
-
-http.createServer(handleRequest).listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Hello World");
+});
