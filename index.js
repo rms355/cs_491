469b27f5713456a8cd572db4d733c0d6d83e3548
rms355 <rms355@gmail.com>
2020-08-01 00:39:27 -0500

    Re-added index.htm to index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.static('pages'));
 app.use('/static', express.static(path.join(__dirname, 'pages')));
 
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/index.htm'));
-// });
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.htm'));
+});
 
 
 const port = process.env.PORT || 3000;
